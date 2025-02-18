372d0a052d1bf26933b0433cc87896e6afe79557
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-02-18 20:20:00 +0300

    add category api controller

---
+++ Presentation/MovieReview.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieReview.Application.Features.CQRS.Commands.CategoryCommands;
+using MovieReview.Application.Features.CQRS.Handlers.CategoryHandlers;
+using MovieReview.Application.Features.CQRS.Queries.CategoryQueries;
+
+namespace MovieReview.WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly GetCategoryQueryHandler _getCategoryQueryHandler;
+        private readonly GetCategoryByIdQueryHandler _getCategoryByIdQueryHandler;
+        private readonly CreateCategoryCommandHandler _createCategoryCommandHandler;
+        private readonly UpdateCategoryCommandHandler _updateCategoryCommandHandler;
+        private readonly RemoveCategoryCommandHandler _removeCategoryCommandHandler;
+
+        public CategoriesController(GetCategoryQueryHandler getCategoryQueryHandler, GetCategoryByIdQueryHandler getCategoryByIdQueryHandler, CreateCategoryCommandHandler createCategoryCommandHandler, UpdateCategoryCommandHandler updateCategoryCommandHandler, RemoveCategoryCommandHandler removeCategoryCommandHandler)
+        {
+            _getCategoryQueryHandler = getCategoryQueryHandler;
+            _getCategoryByIdQueryHandler = getCategoryByIdQueryHandler;
+            _createCategoryCommandHandler = createCategoryCommandHandler;
+            _updateCategoryCommandHandler = updateCategoryCommandHandler;
+            _removeCategoryCommandHandler = removeCategoryCommandHandler;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CategoryList()
+        {
+            var value = await _getCategoryQueryHandler.Handle();
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryCommand command)
+        {
+            await _createCategoryCommandHandler.Handle(command);
+            return Ok("kategori eklendi.");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCategoryById(GetCategoryByIdQuery query)
+        {
+            var value = await _getCategoryByIdQueryHandler.Handle(query);
+            return Ok(value);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCategory(RemoveCategoryCommand command)
+        {
+            await _removeCategoryCommandHandler.Handle(command);
+            return Ok("Kategori silindi.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryCommand command)
+        {
+            await _updateCategoryCommandHandler.Handle(command);
+            return Ok("Kategori Güncellendi.");
+        }
+    }
+}
