489fa6b69fc493ff0c7fec03d1f7e392383e9729
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-03-07 14:26:25 +0300

    Add constructors to query classes and clean up usings

--- Core/MovieReview.Application/Features/CQRS/Queries/CategoryQueries/GetCategoryByIdQuery.cs
+++ Core/MovieReview.Application/Features/CQRS/Queries/CategoryQueries/GetCategoryByIdQuery.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MovieReview.Application.Features.CQRS.Queries.CategoryQueries
+﻿namespace MovieReview.Application.Features.CQRS.Queries.CategoryQueries
 {
     public class GetCategoryByIdQuery
     {
         public int Id { get; set; }
+
+        public GetCategoryByIdQuery(int id)
+        {
+            Id = id;
+        }
     }
 }
--- Core/MovieReview.Application/Features/CQRS/Queries/MovieQueries/GetMovieByIdQuery.cs
+++ Core/MovieReview.Application/Features/CQRS/Queries/MovieQueries/GetMovieByIdQuery.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MovieReview.Application.Features.CQRS.Queries.MovieQueries
+﻿namespace MovieReview.Application.Features.CQRS.Queries.MovieQueries
 {
     public class GetMovieByIdQuery
     {
         public int MovieId { get; set; }
-       
+
+        public GetMovieByIdQuery(int movieId)
+        {
+            MovieId = movieId;
+        }
     }
 }
