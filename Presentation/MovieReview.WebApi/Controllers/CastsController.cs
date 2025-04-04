0aef7af00f2b719b7bba74eef66cbee3305cff79
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-04-04 21:58:13 +0300

    add api controller

---
+++ Presentation/MovieReview.WebApi/Controllers/CastsController.cs
@@ -0,0 +1,66 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieReview.Application.Features.Mediator.Commands.CastCommands;
+using MovieReview.Application.Features.Mediator.Queries.CastQueries;
+
+namespace MovieReview.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CastsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CastsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("GetCastList")]
+        public async Task<IActionResult> GetCastList()
+        {
+            var result = await _mediator.Send(new GetCastQuery());
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        [HttpPost("CreateCast")]
+        public async Task<IActionResult> CreateCast([FromBody] CreateCastCommand command)
+        {
+            await _mediator.Send(command);
+
+            return Ok("Ekleme Başarılı");
+        }
+
+        [HttpDelete("DeleteCast")]
+        public async Task<IActionResult> DeleteCast(int id)
+        {
+            var command = new RemoveCastCommand(id);
+            await _mediator.Send(command);
+            return Ok("Silme Başarılı");
+        }
+
+        [HttpGet("GetCastById")]
+        public async Task<IActionResult> GetCastById(int id)
+        {
+            var result = await _mediator.Send(new GetCastByIdQuery(id));
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        [HttpPut("UpdateCast")]
+        public async Task<IActionResult> UpdateCast(UpdateCastCommand command)
+        {
+            await _mediator.Send(command);
+
+            return Ok("güncelleme başarılı");
+        }
+    }
+}
---
+++ Presentation/MovieReview.WebApi/Controllers/TagsController.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MovieReview.Application.Features.Mediator.Commands.TagCommands;
+using MovieReview.Application.Features.Mediator.Queries.TagQueries;
+
+namespace MovieReview.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TagsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("GetAllTags")]
+        public async Task<IActionResult> GetAllTags()
+        {
+            var result = await _mediator.Send(new GetTagQuery());
+            return Ok(result);
+        }
+
+        [HttpPost("CreateTag")]
+        public async Task<IActionResult> CreateTag([FromBody] CreateTagCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Ekleme Başarılı");
+        }
+
+        [HttpDelete("RemoveTag")]
+        public async Task<IActionResult> RemoveTag(RemoveTagCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Silme Başarılı");
+        }
+
+        [HttpPut("UpdateTag")]
+        public async Task<IActionResult> UpdateTag(UpdateTagCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Güncelleme Başarılı");
+        }
+    }
+}
