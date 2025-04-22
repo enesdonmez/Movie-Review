42f867ea5a32741d5e34fd217bac58c3fd398bc7
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-04-22 21:54:03 +0300

    Add Movie Features

---
+++ Frontend/MovieReview.Web/Controllers/MovieController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieReview.Dto.Dtos.MovieDtos;
+using Newtonsoft.Json;
+
+namespace MovieReview.Web.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MovieController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> MovieList()
+        {
+            ViewBag.v1 = "Film Listesi";
+            ViewBag.v2 = "Ana Sayfa";
+            ViewBag.v3 = "Tüm Filmler";
+
+            var client = _httpClientFactory.CreateClient("MovieReviewAPI");
+            var response = await client.GetAsync("Movies/MovieList");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultMovieDto>>(jsonData);
+                return View(values);
+            }
+
+
+            return View();
+        }
+    }
+}
