fbbf44ca63ac9b0df9e2388b90c5a6d11d1c526e
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-06-14 20:59:14 +0300

    related movie entity

---
+++ Core/MovieReview.Domain/Entities/RelatedMovie.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieReview.Domain.Entities
+{
+    public class RelatedMovie
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public int UserId { get; set; }
+        public bool IsWatch { get; set; }
+    }
+}
